07a58e227e745cd894488300115d2c18c915da3a
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-13 22:02:02 -0300

    Add samples to multi-line string

--- Day001.playground/Contents.swift
+++ Day001.playground/Contents.swift
@@ -10,3 +10,19 @@ var numberOfMessages = 10
 // int can be used with underscore as thousands separator
 var balance = 10_000_000
 
+// multi-line string with line break
+var emailContent = """
+Hello, John!
+
+My name is Samantha and I have an excellent opportunity to you to work in our COBOL team!
+
+Are you evaluating other opportunities right now?
+"""
+
+// multi-line string without line break
+var bigContent = """
+This is\
+a very "large"\
+content without line breaks\
+when I break the line into editor.
+"""
