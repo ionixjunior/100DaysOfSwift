bca04c19fce1495313cc15fed6845c91380eab04
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-13 21:50:44 -0300

    Add samples of int

--- Day001.playground/Contents.swift
+++ Day001.playground/Contents.swift
@@ -4,4 +4,9 @@ import UIKit
 var description = "You have 1 message"
 description = "You have 2 messages"
 
+// int
+var numberOfMessages = 10
+
+// int can be used with underscore as thousands separator
+var balance = 10_000_000
 
