d209e85d6268515bc51749d6c67683d421d892d8
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-14 18:25:19 -0300

    Add sample of array

---
+++ Day002.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// arrays
+let ferrari = "Ferrari"
+let mclaren = "McLaren"
+let mercedes = "Mercedes"
+let cars = [ferrari, mclaren, mercedes]
+cars[0]
+cars[1]
+let bicycles: [String] = ["Caloi", "Sundonw", "Houston"]
