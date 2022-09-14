6020dec8f8c00207c74708f831612779a8da9103
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-14 18:49:58 -0300

    Add sample of tuple

--- Day002.playground/Contents.swift
+++ Day002.playground/Contents.swift
@@ -11,3 +11,10 @@ let bicycles: [String] = ["Caloi", "Sundonw", "Houston"]
 
 // sets
 let tires = Set(["Goodyer", "Michelin", "Goodyer", "GOODYER", "Bridgestone"])
+
+// tuples
+let name = (first: "Ayrton", last: "Senna")
+name.0
+name.1
+name.first
+name.last
