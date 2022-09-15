7b56ad2b20721d2458958ef5fb64a68312734ac3
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-15 18:42:52 -0300

    Add sample of arithmetic operators

---
+++ Day003.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// arithmetic operators
+let value1 = 10
+let value2 = 20
+let sum = value1 + value2
+let sub = value1 - value2
+let division = value1 / value2
+let multiply = value1 * value2
+let daysOfYear = 365
+let daysOfWeek = 7
+let weeksOfYear = daysOfYear / daysOfWeek
+let isMultiple = 30.isMultiple(of: 4)
+
