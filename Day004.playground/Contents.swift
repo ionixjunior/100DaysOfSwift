67eaede30130ae514accf08a1361e1cde52d9967
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-16 18:50:22 -0300

    Add samples of for loops

---
+++ Day004.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// for loops
+let names = ["John", "Samantha"]
+for name in names {
+    print("The name is \(name)")
+}
+
+let numbers = 1...5
+for _ in numbers {
+    print("More one number")
+}
+
