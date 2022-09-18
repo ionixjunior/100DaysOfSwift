ced0de83808b4d3340efb942e74f46a96b56f95c
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-17 22:19:01 -0300

    Add samples of returning values

--- Day005.playground/Contents.swift
+++ Day005.playground/Contents.swift
@@ -13,3 +13,25 @@ func sum(firstValue: Int, secondValue: Int) {
 }
 
 sum(firstValue: 1, secondValue: 5)
+
+
+// returning values
+func getSum(firstValue: Int, secondValue: Int) -> String {
+    let result = firstValue + secondValue
+    return "The sum is \(result)"
+}
+getSum(firstValue: 1, secondValue: 5)
+
+func divisibleBy2(number: Int) -> Bool {
+    number % 2 == 0
+}
+divisibleBy2(number: 7)
+
+func getUsername() -> (first: String, last: String) {
+    ("John", "Smith")
+}
+let username = getUsername()
+print("\(username.first) \(username.last)")
+
+
+
