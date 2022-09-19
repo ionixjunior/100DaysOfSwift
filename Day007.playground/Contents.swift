63c2c03872738d4117254302c629fa37b8a8827f
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-19 18:34:18 -0300

    Add sample of closures returning values

--- Day007.playground/Contents.swift
+++ Day007.playground/Contents.swift
@@ -8,3 +8,14 @@ func showNumberOfMessages(action: (Int) -> Void) {
 showNumberOfMessages { (number: Int) in
     print("You have \(number) messages.")
 }
+
+
+// closures returning values
+func getNumberOfMessages(action: (Int) -> String) {
+    let result = action(10)
+    print(result)
+}
+
+getNumberOfMessages { (number: Int) -> String in
+    return "You have \(number) messages."
+}
