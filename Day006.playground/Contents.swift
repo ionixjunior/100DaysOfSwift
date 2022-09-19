618e979fb3c8f4b2e63c74901d832f6b5ed11997
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-18 21:11:56 -0300

    Add sample with parameters in a closure

--- Day006.playground/Contents.swift
+++ Day006.playground/Contents.swift
@@ -5,3 +5,10 @@ let showMessages = {
     print("You have 3 new messages")
 }
 showMessages()
+
+
+// accepting parameters in a closure
+let showNameAndNumberOfMessages = { (name: String, number: Int) in
+    print("Hello \(name)! You have \(number) new messages!")
+}
+showNameAndNumberOfMessages("John", 5)
