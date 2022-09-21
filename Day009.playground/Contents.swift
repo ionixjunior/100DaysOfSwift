4d184c3a5a059c8ec2e50dd6c7acba9952f2788e
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-21 19:07:04 -0300

    Add sample of initializers

---
+++ Day009.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// initializers
+struct User {
+    var username: String
+}
+
+extension User {
+    init() {
+        username = "Anonymous"
+    }
+}
+
+var laura = User(username: "Laura")
+laura.username
+
+var samantha = User()
+samantha.username = "Samantha"
+samantha.username
+
+
