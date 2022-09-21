717e48cc2129cca8f14444347fffc87c6b48115e
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-21 20:00:51 -0300

    Add sample of access control

--- Day009.playground/Contents.swift
+++ Day009.playground/Contents.swift
@@ -32,3 +32,20 @@ var anonimous = User()
 User.numberOfUsers
 
 
+// access control
+struct Product {
+    private var id: Int
+    var name: String
+    
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+    
+    func getIdentifier() -> String {
+        "The product ID is \(id)"
+    }
+}
+
+let product = Product(id: 1, name: "Bicycle")
+product.getIdentifier()
