6a990b22e115bacdef7355a9535a25005b8b8433
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-22 18:32:02 -0300

    Add sample of classes

---
+++ Day010.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// creating classes
+class Dog {
+    var name: String
+    var breed: String
+    
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+var dog = Dog(name: "Teco", breed: "Yorkshire")
