48b6536fabdbb1c7eda17a9d09b6ab0c7f7419ae
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-24 10:11:54 -0300

    Add sample of unwrapping optionals

--- Day012.playground/Contents.swift
+++ Day012.playground/Contents.swift
@@ -3,7 +3,20 @@ import UIKit
 // handling missing data
 var name: String?
 var age: Int? = nil
+
+
+// unwrapping optionals
+func unwrap(value: String?) {
+    if let unwrapped = value {
+        print("The value is \(unwrapped)")
+    } else {
+        print("The value is nil")
+    }
+}
+
+unwrap(value: name)
 name = "Samantha"
 age = 35
+unwrap(value: name)
 
 
