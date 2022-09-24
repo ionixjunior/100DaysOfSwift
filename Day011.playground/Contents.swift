070ba3ecc1d0322d0215a99eee9f4765d95a8d10
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-23 22:07:10 -0300

    Add sample of protocol inheritance

--- Day011.playground/Contents.swift
+++ Day011.playground/Contents.swift
@@ -4,13 +4,36 @@ protocol Identifiable {
     var id: Int { get set }
 }
 
-struct User: Identifiable {
+protocol FirstName {
+    var first: String { get set }
+}
+
+protocol LastName {
+    var last: String { get set }
+}
+
+protocol Fullname: FirstName, LastName {
+    func getFullname() -> String
+}
+
+struct User: Identifiable, Fullname {
     var id: Int
+    var first: String
+    var last: String
+    
+    func getFullname() -> String {
+        "\(first) \(last)"
+    }
 }
 
 func displayID(_ identifiable: Identifiable) {
     print("The ID is \(identifiable.id)")
 }
 
-let user = User(id: 1)
+func displayFullname(_ fullname: Fullname) {
+    print("The full name is \(fullname.getFullname())")
+}
+
+let user = User(id: 1, first: "Samantha", last: "Miller")
 displayID(user)
+displayFullname(user)
