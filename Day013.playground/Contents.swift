663d87aa79555209080907b5aeff066038d3dd09
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-25 10:30:31 -0300

    Add sample of accuracy of numbers

---
+++ Day013.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+// review about accuracy of numbers
+let floatNumber: Float = 123.12345678987654321
+let doubleNumber: Double = 123.12345678987654321
+let decimalNumber: Decimal = 123.12345678987654321
