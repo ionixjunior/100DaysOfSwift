0217be42ae1f16868c4a3cc25ab3c5bf2f843a3d
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-25 10:38:02 -0300

    Add sample of range and switch

--- Day013.playground/Contents.swift
+++ Day013.playground/Contents.swift
@@ -10,3 +10,20 @@ let decimalNumber: Decimal = 123.12345678987654321
 let arrayA = ["John", "Samantha"]
 let arrayB = ["Jorge", "Laura", "Dayana"]
 let arrayC = arrayA + arrayB
+
+
+// review about range and switch
+let range1 = 0...10
+let range2 = 11...20
+type(of: range1)
+
+let value = 13
+
+switch value {
+case range1:
+    print("Range 1 selected!")
+case range2:
+    print("Range 2 selected!")
+default:
+    print("Unknown range")
+}
