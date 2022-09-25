86432b9d725ec1c62fdd7b848a19bcfc5fbc622a
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-25 10:32:59 -0300

    Add sample of array concat

--- Day013.playground/Contents.swift
+++ Day013.playground/Contents.swift
@@ -4,3 +4,9 @@ import UIKit
 let floatNumber: Float = 123.12345678987654321
 let doubleNumber: Double = 123.12345678987654321
 let decimalNumber: Decimal = 123.12345678987654321
+
+
+// review about array concat
+let arrayA = ["John", "Samantha"]
+let arrayB = ["Jorge", "Laura", "Dayana"]
+let arrayC = arrayA + arrayB
