361065f61506eb599ae0db17debebf2ff1c5bfae
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-26 18:44:23 -0300

    Add sample of implicitly unwrapped optionals

---
+++ Day014.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+// review of optionals
+var name1: String = "John"
+var name2: String?
+var name3: String! // implicitly unwrapped optionals
+print("The name 1 is " + name1)
+//print("The name 2 is " + name2) // Xcode will tell us that we need to unwrap the optional
+//print("The name 3 is " + name3) // Xcode will tell us that the unwrapped value is nil
