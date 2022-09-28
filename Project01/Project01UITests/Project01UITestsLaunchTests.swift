eb1a33ae1cecf928a9bc67eeeb3b25c2debb1b17
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-28 07:42:31 -0300

    Create project 1

---
+++ Project01/Project01UITests/Project01UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Project01UITestsLaunchTests.swift
+//  Project01UITests
+//
+//  Created by Ione Souza Junior on 28/09/22.
+//
+
+import XCTest
+
+class Project01UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
