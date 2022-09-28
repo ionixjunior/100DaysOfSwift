90eb081e79a1c951c14ee9b6b31a5fdbec5a5e38
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-28 08:35:12 -0300

    Implement number of rows in section and cell for row at
    An interesting thing is that cellForRowAt change in iOS 14. Now exists a different way to update the cell and will be replaced in the future https://developer.apple.com/documentation/uikit/uilistcontentconfiguration. Xcode shows that cell.textLabel will be deprecated.

--- Project01/Project01/ViewController.swift
+++ Project01/Project01/ViewController.swift
@@ -19,6 +19,25 @@ class ViewController: UITableViewController {
         print(pictures)
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        return updateCell(cell, text: pictures[indexPath.row])
+    }
+    
+    private func updateCell(_ cell: UITableViewCell, text: String) -> UITableViewCell {
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = text
+            cell.contentConfiguration = content
+            return cell
+        }
+        
+        cell.textLabel?.text = text
+        return cell
+    }
 }
 
