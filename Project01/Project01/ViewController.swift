4367e12ff1cd301ec4600c1a950957ba7db04b45
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-28 07:59:58 -0300

    Add prefixed images to pictures array

--- Project01/Project01/ViewController.swift
+++ Project01/Project01/ViewController.swift
@@ -1,10 +1,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fileManager = FileManager.default
+        let resourcePath = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: resourcePath)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
     }
 
 
