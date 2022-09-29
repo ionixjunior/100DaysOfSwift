db78f464f41417304b7fbfc04e8a883d4105f521
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-29 18:13:05 -0300

    Navigate to the DetailViewController

--- Project01/Project01/ViewController.swift
+++ Project01/Project01/ViewController.swift
@@ -47,5 +47,12 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = text
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
