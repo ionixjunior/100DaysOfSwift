920081b01efbf9b039724b6aec981a9c969f1ced
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-29 18:40:17 -0300

    Disable large title into DetailViewController

--- Project01/Project01/DetailViewController.swift
+++ Project01/Project01/DetailViewController.swift
@@ -7,6 +7,7 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
         
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
