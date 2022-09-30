556fe4db4d618ba3faf12c6241a7a66855ffbf41
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-30 20:56:23 -0300

    Add testes to check prefix of content

--- Project01/Project01Tests/PictureLoaderTests.swift
+++ Project01/Project01Tests/PictureLoaderTests.swift
@@ -2,4 +2,14 @@ import XCTest
 @testable import Project01
 
 class PictureLoaderTests: XCTestCase {
+    let content: [String] = ["item1", "item2", "nssl2", "nssl1"]
+    
+    func testLoadOnlyPicturesPrefixedWithNssl() throws {
+        let pictures = PictureLoader.loadPicturesFrom(content: content)
+        
+        XCTAssertNotEqual("item1", pictures[0].name)
+        XCTAssertNotEqual("item2", pictures[0].name)
+        XCTAssertNotEqual("item1", pictures[1].name)
+        XCTAssertNotEqual("item2", pictures[1].name)
+    }
 }
