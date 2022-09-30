6834db1faf8d412c617e8f8c16eb237d5cb3d502
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-30 20:46:02 -0300

    Add PictureLoader and test class

---
+++ Project01/Project01/PictureLoader.swift
@@ -0,0 +1,5 @@
+struct PictureLoader {
+    static func loadPicturesFrom(content: [String]) -> [Picture] {
+        [Picture]()
+    }
+}
---
+++ Project01/Project01Tests/PictureLoaderTests.swift
@@ -0,0 +1,5 @@
+import XCTest
+@testable import Project01
+
+class PictureLoaderTests: XCTestCase {
+}
