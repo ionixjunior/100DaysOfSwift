19e0d068c0ae21b0200c2b9c99ada74d33463f69
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-30 20:59:42 -0300

    Implement load pictures filtering by prefix

--- Project01/Project01/PictureLoader.swift
+++ Project01/Project01/PictureLoader.swift
@@ -1,5 +1,15 @@
 struct PictureLoader {
     static func loadPicturesFrom(content: [String]) -> [Picture] {
-        [Picture]()
+        var pictures = [Picture]()
+        
+        var position = 1
+        for picture in content {
+            if picture.hasPrefix("nssl") {
+                pictures.append(Picture(position: position, name: picture))
+                position += 1
+            }
+        }
+        
+        return pictures
     }
 }
