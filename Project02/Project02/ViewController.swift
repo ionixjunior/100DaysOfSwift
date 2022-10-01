a89da4eac311e5815e34ba52a057677172610055
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-01 18:11:13 -0300

    Add button border

--- Project02/Project02/ViewController.swift
+++ Project02/Project02/ViewController.swift
@@ -19,8 +19,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
-        
+        addButtonBorder()
         askQuestion()
+    }
+    
+    func addButtonBorder() {
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
     }
     
     func askQuestion() {
