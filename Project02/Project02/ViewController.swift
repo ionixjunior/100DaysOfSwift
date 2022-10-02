9ba75cc811d2b9b4a486d6748581de786d0a61de
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-02 20:23:27 -0300

    Check if the flag tapped is correct

--- Project02/Project02/ViewController.swift
+++ Project02/Project02/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
         button3.layer.borderColor = UIColor.lightGray.cgColor
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         button1.setImage(UIImage(named: countries[0]), for: .normal)
@@ -43,6 +43,19 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let alert = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(alert, animated: true)
     }
 }
 
