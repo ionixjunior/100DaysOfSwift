5593eb10bcde792c98dd1f64fd380478da689020
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-02 19:50:50 -0300

    Shuffle countries array

--- Project02/Project02/ViewController.swift
+++ Project02/Project02/ViewController.swift
@@ -33,6 +33,7 @@ class ViewController: UIViewController {
     }
     
     func askQuestion() {
+        countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
