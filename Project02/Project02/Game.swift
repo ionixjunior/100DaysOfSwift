05416105f1042b4c21d827581b45cafc1c1d6ad6
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-03 21:13:48 -0300

    Implement a tuple return tu get three options when start the game

--- Project02/Project02/Game.swift
+++ Project02/Project02/Game.swift
@@ -16,8 +16,11 @@ class Game {
         _answeredQuestions
     }
     
-    func start() {
+    func start() -> (option1: String, option2: String, option3: String) {
         _score = 0
         _answeredQuestions = 0
+        _options.shuffle()
+        _correctAnswer = Int.random(in: 0...2)
+        return (option1: _options[0], option2: _options[1], option3: _options[2])
     }
 }
--- Project02/Project02Tests/GameTests.swift
+++ Project02/Project02Tests/GameTests.swift
@@ -3,20 +3,29 @@ import XCTest
 
 final class GameTests: XCTestCase {
     var game: Game!
+    let options = ["Option 1", "Option 2", "Option 3"]
     
     override func setUpWithError() throws {
-        game = Game(options: ["Option 1", "Option 2", "Option 3"])
+        game = Game(options: options)
     }
     
     func testStartShouldGetScore() throws {
-        game.start()
+        _ = game.start()
         
         XCTAssertEqual(0, game.score)
     }
     
     func testStartShouldGetAnsweredQuestions() throws {
-        game.start()
+        _ = game.start()
         
         XCTAssertEqual(0, game.answeredQuestions)
+    }
+    
+    func testStartShouldGetThreeOptions() throws {
+        let result = game.start()
+        
+        XCTAssertTrue(options.contains(result.option1))
+        XCTAssertTrue(options.contains(result.option2))
+        XCTAssertTrue(options.contains(result.option3))
     }
 }
