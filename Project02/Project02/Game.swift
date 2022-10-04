826a49e4c3d674ebc59fbcd9f93525359f438fc1
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-03 22:14:41 -0300

    Call nextQuestion in start method

--- Project02/Project02/Game.swift
+++ Project02/Project02/Game.swift
@@ -24,9 +24,7 @@ class Game {
     func start() -> (option1: String, option2: String, option3: String) {
         _score = 0
         _answeredQuestions = 0
-        _options.shuffle()
-        _correctAnswer = Int.random(in: 0...2)
-        return (option1: _options[0], option2: _options[1], option3: _options[2])
+        return nextQuestion()
     }
     
     func answer(_ answer: Int) -> Bool {
