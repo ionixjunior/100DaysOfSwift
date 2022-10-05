841ce03cc4338bdc2f7357990478412d7f80afeb
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-05 19:00:40 -0300

    Add countries into array

--- MilestoneProjects01-03/MilestoneProjects01-03/ViewController.swift
+++ MilestoneProjects01-03/MilestoneProjects01-03/ViewController.swift
@@ -6,9 +6,24 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        addCountries()
     }
-
-
+    
+    func addCountries() {
+        countries += [
+            Country(name: "Estonia", flag: "estonia"),
+            Country(name: "France", flag: "france"),
+            Country(name: "Germany", flag: "germany"),
+            Country(name: "Ireland", flag: "ireland"),
+            Country(name: "Italy", flag: "italy"),
+            Country(name: "Monaco", flag: "monaco"),
+            Country(name: "Nigeria", flag: "nigeria"),
+            Country(name: "Poland", flag: "poland"),
+            Country(name: "Russia", flag: "russia"),
+            Country(name: "Spain", flag: "spain"),
+            Country(name: "UK", flag: "uk"),
+            Country(name: "US", flag: "us")
+        ]
+    }
 }
 
