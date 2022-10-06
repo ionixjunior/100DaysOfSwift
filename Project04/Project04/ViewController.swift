f57bc417c3b2f22439dbbc98f03e2da76faf2a5b
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-06 08:04:46 -0300

    Add right bar button to open an action sheet with pages to navigate

--- Project04/Project04/ViewController.swift
+++ Project04/Project04/ViewController.swift
@@ -14,10 +14,23 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         let url = URL(string: "https://ionixjunior.dev")!
         webView.load(URLRequest(url: url))
     }
-
-
+    
+    @objc func openTapped() {
+        let alertController = UIAlertController(title: "Choose a page to open", message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "ionixjunior.dev", style: .default, handler: pageTapped))
+        alertController.addAction(UIAlertAction(title: "apple.com", style: .default, handler: pageTapped))
+        alertController.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: pageTapped))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(alertController, animated: true)
+    }
+    
+    func pageTapped(action: UIAlertAction) {
+        
+    }
 }
 
