c34fd0ab613bc8a629f6ebbfa7f021d488dd415c
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-06 08:18:21 -0300

    Implement webView didFinish method
    This method will show the title of the web view into title of navigation controller

--- Project04/Project04/ViewController.swift
+++ Project04/Project04/ViewController.swift
@@ -42,5 +42,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         webView.load(URLRequest(url: url))
     }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
 
