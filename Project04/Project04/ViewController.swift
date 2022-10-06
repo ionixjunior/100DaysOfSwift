96142b6f55fb1d7dc0483c1239198c3469c64f7e
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-06 07:45:00 -0300

    Load a web view page into loadView method

--- Project04/Project04/ViewController.swift
+++ Project04/Project04/ViewController.swift
@@ -1,8 +1,17 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
+    
+    override func loadView() {
+        let url = URL(string: "https://ionixjunior.dev")!
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        webView.allowsBackForwardNavigationGestures = true
+        webView.load(URLRequest(url: url))
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
