7a0c67aa21b44f350b88d55ad56e7c01cb163447
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-10 18:21:06 -0300

    Create project 5

---
+++ Project05/Project05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project05
+//
+//  Created by Ione Souza Junior on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
