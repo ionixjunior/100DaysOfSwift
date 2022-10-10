bb1e924a36ef86b7587be3f8245e87e9ad056831
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-10 18:43:04 -0300

    Load all words from start file

--- Project05/Project05/ViewController.swift
+++ Project05/Project05/ViewController.swift
@@ -1,9 +1,16 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var allWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
     }
 
 
