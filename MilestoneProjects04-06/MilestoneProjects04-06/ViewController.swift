28012e20da82b7c6f17f829800dd4b6bd6b11f35
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-15 09:18:57 -0300

    Implement cell for row at method

--- MilestoneProjects04-06/MilestoneProjects04-06/ViewController.swift
+++ MilestoneProjects04-06/MilestoneProjects04-06/ViewController.swift
@@ -11,5 +11,11 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return shoppingList.total
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.update(text: shoppingList.itemFrom(position: indexPath.row))
+        return cell
+    }
 }
 
