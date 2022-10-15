71797d278f769c8f1258378b5052694372e27f37
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-15 09:49:07 -0300

    Implement add itens into list

--- MilestoneProjects04-06/MilestoneProjects04-06/ViewController.swift
+++ MilestoneProjects04-06/MilestoneProjects04-06/ViewController.swift
@@ -6,7 +6,21 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Shopping list"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
         shoppingList = ShoppingList()
+    }
+    
+    @objc func addTapped() {
+        let alert = UIAlertController(title: "What do you want to add?", message: nil, preferredStyle: .alert)
+        alert.addTextField()
+        alert.addAction(UIAlertAction(title: "Add", style: .default) { [weak alert] _ in
+            if let item = alert?.textFields?[0].text {
+                self.shoppingList.add(item: item)
+                self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+            }
+        })
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(alert, animated: true)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
