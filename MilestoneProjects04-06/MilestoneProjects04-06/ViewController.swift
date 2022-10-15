ce8de619d4b93bee8bdf0faf1771166d9398e886
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-15 09:09:18 -0300

    Create shopping list object into view did load

--- MilestoneProjects04-06/MilestoneProjects04-06/ViewController.swift
+++ MilestoneProjects04-06/MilestoneProjects04-06/ViewController.swift
@@ -1,10 +1,11 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    private var shoppingList: ShoppingList!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        shoppingList = ShoppingList()
     }
 }
 
