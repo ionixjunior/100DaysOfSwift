1df1df284fd335958592c7618af3ba67e0d4cb2f
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-15 11:22:37 -0300

    Implement a simple UI test
    This test do:
    
    - Add items into the table view and validated if the items appear;
    - Check if the share sheet papers;
    - Clear the list of items.

--- MilestoneProjects04-06/MilestoneProjects04-06UITests/MilestoneProjects04_06UITests.swift
+++ MilestoneProjects04-06/MilestoneProjects04-06UITests/MilestoneProjects04_06UITests.swift
@@ -9,9 +9,27 @@ final class MilestoneProjects04_06UITests: XCTestCase {
     override func tearDownWithError() throws {
     }
 
-    func testExample() throws {
+    func testApp() throws {
         let app = XCUIApplication()
         app.launch()
-
+        
+        let item1 = "Potato"
+        let item2 = "Tomato"
+        app.navigationBars.buttons["Add"].tap()
+        app.textFields.firstMatch.typeText(item1)
+        app.alerts.buttons["Add"].tap()
+        XCTAssertTrue(app.tables.firstMatch.cells.element(boundBy: 0).staticTexts[item1].exists)
+        
+        app.navigationBars.buttons["Add"].tap()
+        app.textFields.firstMatch.typeText(item2)
+        app.alerts.buttons["Add"].tap()
+        XCTAssertTrue(app.tables.firstMatch.cells.element(boundBy: 0).staticTexts[item2].exists)
+        
+        app.buttons["Share"].tap()
+        app.buttons["Close"].tap()
+        
+        app.navigationBars.buttons["Delete"].tap()
+        app.alerts.buttons["Yes"].tap()
+        XCTAssertFalse(app.tables.cells.element(boundBy: 0).exists)
     }
 }
