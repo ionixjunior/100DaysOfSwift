31a1500c598eac467735536b3abdbebc13564262
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-16 10:22:10 -0300

    Load data from URL

--- Project07/Project07/ViewController.swift
+++ Project07/Project07/ViewController.swift
@@ -1,9 +1,23 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    private var petitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let url = URL(string: "https://www.hackingwithswift.com/samples/petitions-1.json") {
+            if let data = try? Data(contentsOf: url) {
+                parse(data: data)
+            }
+        }
+    }
+    
+    private func parse(data: Data) {
+        let jsonDecoder = JSONDecoder()
+        if let decodedPetitions = try? jsonDecoder.decode(Petitions.self, from: data) {
+            petitions = decodedPetitions.results
+        }
     }
 }
 
