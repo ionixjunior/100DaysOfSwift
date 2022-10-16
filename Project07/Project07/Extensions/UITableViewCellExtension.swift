56ceff008099274f5712e27d213223b3b4c47e17
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-16 10:39:39 -0300

    Change text properties into cell
    I added line break mode property to title and subtitle into content configuration.

--- Project07/Project07/Extensions/UITableViewCellExtension.swift
+++ Project07/Project07/Extensions/UITableViewCellExtension.swift
@@ -15,7 +15,11 @@ extension UITableViewCell {
         if #available(iOS 14.0, *) {
             var contentConfiguration = self.defaultContentConfiguration()
             contentConfiguration.text = title
+            contentConfiguration.textProperties.lineBreakMode = .byTruncatingTail
+            contentConfiguration.textProperties.numberOfLines = 1
             contentConfiguration.secondaryText = subtitle
+            contentConfiguration.secondaryTextProperties.lineBreakMode = .byTruncatingTail
+            contentConfiguration.secondaryTextProperties.numberOfLines = 3
             self.contentConfiguration = contentConfiguration
         } else {
             self.textLabel?.text = title
