ccb44a5244692afd36217185761169928adc079b
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-17 07:59:56 -0300

    Load data based on selected tab
    For this I using the tag of the tab bar item.

--- Project07/Project07/ViewController.swift
+++ Project07/Project07/ViewController.swift
@@ -14,14 +14,23 @@ class ViewController: UITableViewController {
     @objc private func loadData() {
         tableView.refreshControl?.beginRefreshing()
         tableView.refreshControl?.attributedTitle = NSAttributedString(string: "Loading data...")
+        let urlString = getUrlString()
         
         DispatchQueue.global(qos: .userInitiated).async {
-            if let url = URL(string: "https://www.hackingwithswift.com/samples/petitions-1.json") {
+            if let url = URL(string: urlString) {
                 if let data = try? Data(contentsOf: url) {
                     self.parse(data: data)
                 }
             }
         }
+    }
+    
+    private func getUrlString() -> String {
+        if navigationController?.tabBarItem.tag == 0 {
+            return "https://www.hackingwithswift.com/samples/petitions-1.json"
+        }
+        
+        return "https://www.hackingwithswift.com/samples/petitions-2.json"
     }
     
     private func parse(data: Data) {
