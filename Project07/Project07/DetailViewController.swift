eb82f3e56b704109729d1da899d9f9e70d2a1869
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-17 07:24:47 -0300

    Load custom HTML data into webview

--- Project07/Project07/DetailViewController.swift
+++ Project07/Project07/DetailViewController.swift
@@ -12,5 +12,21 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let detailItem = detailItem else { return }
+        
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
     }
 }
