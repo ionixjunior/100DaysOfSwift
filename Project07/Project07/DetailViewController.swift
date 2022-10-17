f6cd47cec8eba69918cf80f61e512cc528664d59
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-17 07:19:07 -0300

    Add WKWebView into view

--- Project07/Project07/DetailViewController.swift
+++ Project07/Project07/DetailViewController.swift
@@ -1,7 +1,14 @@
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
+    var webView: WKWebView!
 
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
