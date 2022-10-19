3b8e9588dfc09dc4f30712e0adc91c283cd7fc2d
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-19 07:00:56 -0300

    Add UI properties to view controller

--- Project08/Project08/ViewController.swift
+++ Project08/Project08/ViewController.swift
@@ -1,6 +1,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
