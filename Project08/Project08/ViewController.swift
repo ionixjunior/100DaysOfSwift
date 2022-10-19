2db4dbba9ee17ba888ab562200c0d1ecf4801787
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-19 06:42:56 -0300

    Add project 8

---
+++ Project08/Project08/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project08
+//
+//  Created by Ione Souza Junior on 19/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project08/Project08UITests/Project08UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Project08UITestsLaunchTests.swift
+//  Project08UITests
+//
+//  Created by Ione Souza Junior on 19/10/22.
+//
+
+import XCTest
+
+final class Project08UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
