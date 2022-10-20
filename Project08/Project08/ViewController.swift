2ce0b0f74fafdc9758d2b22e1a31b769b59fef65
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-20 07:41:10 -0300

    Implement letter tapped method

--- Project08/Project08/ViewController.swift
+++ Project08/Project08/ViewController.swift
@@ -116,7 +116,11 @@ class ViewController: UIViewController {
     }
     
     @objc func letterTapped(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else { return }
         
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
     }
     
     @objc func submitTapped(_ sender: UIButton) {
