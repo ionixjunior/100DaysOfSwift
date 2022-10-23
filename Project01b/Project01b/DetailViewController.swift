94a1383294ca2e9c7c0ae0cefabca0ad79120ed7
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-23 09:59:49 -0300

    Add project 1b

---
+++ Project01b/Project01b/DetailViewController.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String!
+    var currentPicture: Int!
+    var totalOfPictures: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Picture \(currentPicture ?? 0) of \(totalOfPictures ?? 0)"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+}
---
+++ Project01b/Project01bTests/PictureLoaderTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import Project01b
+
+class PictureLoaderTests: XCTestCase {
+    let content: [String] = ["item1", "item2", "nssl2", "nssl1"]
+    
+    func testLoadOnlyPicturesPrefixedWithNssl() throws {
+        let pictures = PictureLoader.loadPicturesFrom(content: content)
+        
+        XCTAssertNotEqual("item1", pictures[0].name)
+        XCTAssertNotEqual("item2", pictures[0].name)
+        XCTAssertNotEqual("item1", pictures[1].name)
+        XCTAssertNotEqual("item2", pictures[1].name)
+    }
+    
+    func testSortByName() throws {
+        let pictures = PictureLoader.loadPicturesFrom(content: content)
+        
+        XCTAssertEqual("nssl1", pictures[0].name)
+        XCTAssertEqual("nssl2", pictures[1].name)
+    }
+}
