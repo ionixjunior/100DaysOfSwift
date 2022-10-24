4802bbbc17148388ddb973dda6a3fa0eeb44969f
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-24 20:07:47 -0300

    Check typed letters in getMaskedWord method

--- MilestoneProjects07-09/MilestoneProjects07-09/HangmanGame.swift
+++ MilestoneProjects07-09/MilestoneProjects07-09/HangmanGame.swift
@@ -1,5 +1,6 @@
 class HangmanGame {
     private var word: String!
+    private var typedLetters = [String]()
     
     func start(word: String) {
         self.word = word
@@ -9,9 +10,18 @@ class HangmanGame {
         var maskedWord = ""
         
         for letter in word {
-            maskedWord += "_ "
+            if typedLetters.contains(String(letter)) {
+                maskedWord += "\(letter) "
+            } else {
+                maskedWord += "_ "
+            }
         }
         
         return maskedWord.trimmingCharacters(in: .whitespaces)
+    }
+    
+    func type(letter: String) -> Bool {
+        typedLetters.append(letter)
+        return word.contains(letter)
     }
 }
--- MilestoneProjects07-09/MilestoneProjects07-09Tests/HangmanGameTests.swift
+++ MilestoneProjects07-09/MilestoneProjects07-09Tests/HangmanGameTests.swift
@@ -19,4 +19,22 @@ final class HangmanGameTests: XCTestCase {
         
         XCTAssertEqual("_ _ _ _ _ _ _ _ _", result)
     }
+    
+    func testMaskedWordShouldRevealTypedLetterWhenItIsCorrect() throws {
+        hangmanGame.start(word: word)
+        _ = hangmanGame.type(letter: "e")
+        
+        let result = hangmanGame.getMaskedWord()
+        
+        XCTAssertEqual("_ e _ _ _ _ _ _ e", result)
+    }
+    
+    func testMaskedWordShouldNotRevelAnyLetterWhenItIsIncorrect() throws {
+        hangmanGame.start(word: word)
+        _ = hangmanGame.type(letter: "y")
+        
+        let result = hangmanGame.getMaskedWord()
+        
+        XCTAssertEqual("_ _ _ _ _ _ _ _ _", result)
+    }
 }
