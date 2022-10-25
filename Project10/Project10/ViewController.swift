1b4c6e5911ca94fe73ef0d4a3201f11ea3eefc24
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-25 08:03:52 -0300

    Add cell for item at

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -9,5 +9,10 @@ class ViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else { fatalError("Unable to dequeue PersonCell") }
+        return cell
+    }
 }
 
