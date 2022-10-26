8271393b9eb366056fa5846ecf5b22dbdb78f7f7
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-26 08:10:41 -0300

    Add extension method for URL appending

---
+++ Project10/Project10/Extensions/URLExtension.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension URL {
+    func appending(path: String) -> URL {
+        if #available(iOS 16.0, *) {
+            return self.appending(path: path, directoryHint: .inferFromPath)
+        }
+        
+        return self.appendingPathComponent(path)
+    }
+}
