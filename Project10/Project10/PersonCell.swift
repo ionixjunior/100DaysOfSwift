76a984640253e177f34ab5267f0053eb9ba26699
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-26 07:55:51 -0300

    Implement update method into cell

--- Project10/Project10/PersonCell.swift
+++ Project10/Project10/PersonCell.swift
@@ -4,4 +4,8 @@ class PersonCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var name: UILabel!
     
+    func update(name: String, image: String) {
+        self.name.text = name
+        self.imageView.image = UIImage(contentsOfFile: image)
+    }
 }
