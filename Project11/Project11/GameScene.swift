070f5ee1c10b8c662ecbacea8c68131d9b4f6c9a
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-28 07:40:21 -0300

    Add screen background

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -7,6 +7,8 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        addChild(background)
     }
 }
