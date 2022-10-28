0182c8e3669c81e5501de31b342138153d0a572f
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-28 07:44:08 -0300

    Configure blend mode and zposition
    Blend mode is to ignore background alpha channel and zposition is to render the background behind all elements.

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -9,6 +9,8 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
         addChild(background)
     }
 }
