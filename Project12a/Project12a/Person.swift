67f6f09597fd3fef7bc351b12ab24915543e0a2e
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-31 07:04:58 -0300

    Add project 12a

---
+++ Project12a/Project12a/Person.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
