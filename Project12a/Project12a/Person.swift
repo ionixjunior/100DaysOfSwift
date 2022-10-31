5f91ad87d8586bece8e183b72e9f950db4d63964
Ione Souza Junior <junior@ionixjunior.com.br>
2022-10-31 07:22:51 -0300

    Implement NSCoding protocol into Person class

--- Project12a/Project12a/Person.swift
+++ Project12a/Project12a/Person.swift
@@ -1,11 +1,21 @@
 import UIKit
 
-class Person: NSObject {
+class Person: NSObject, NSCoding {
     var name: String
     var image: String
     
     init(name: String, image: String) {
         self.name = name
         self.image = image
+    }
+    
+    required init?(coder: NSCoder) {
+        name = coder.decodeObject(forKey: "name") as? String ?? ""
+        image = coder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
     }
 }
