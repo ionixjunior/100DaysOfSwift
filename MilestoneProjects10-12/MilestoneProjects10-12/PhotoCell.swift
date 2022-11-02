f6e58655c357446a578135ff3fd8cdd9e6f4a20b
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-02 10:31:30 -0300

    Add layout cell with photo and label

---
+++ MilestoneProjects10-12/MilestoneProjects10-12/PhotoCell.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class PhotoCell: UITableViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
