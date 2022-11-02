7d80e86ede549e157efeb38047fb7efed53c0707
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-02 11:52:58 -0300

    Add storybord id into new view controller

--- MilestoneProjects10-12/MilestoneProjects10-12/DetailViewController.swift
+++ MilestoneProjects10-12/MilestoneProjects10-12/DetailViewController.swift
@@ -1,7 +1,9 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
