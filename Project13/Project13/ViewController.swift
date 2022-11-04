20926a5ad52f781c66ed3e9e022cf2f9ddf8f2bd
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-04 07:34:25 -0300

    Add outlets

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
 }
 
