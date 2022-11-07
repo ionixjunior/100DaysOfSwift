5b33ff8353486594c97cc3b4b795adb05c64f769
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-07 06:56:16 -0300

    Add background

--- Project14/Project14/GameScene.swift
+++ Project14/Project14/GameScene.swift
@@ -2,7 +2,11 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
-        
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
