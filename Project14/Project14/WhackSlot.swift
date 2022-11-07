7493a3643313ea490eda8ce67919bbd7c792dafd
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-07 07:14:12 -0300

    Add penguin into whack slot

--- Project14/Project14/WhackSlot.swift
+++ Project14/Project14/WhackSlot.swift
@@ -2,10 +2,24 @@ import UIKit
 import SpriteKit
 
 class WhackSlot: SKNode {
+    var charNode: SKSpriteNode!
+    
     func configure(at position: CGPoint) {
         self.position = position
         
         let sprite = SKSpriteNode(imageNamed: "whackHole")
         addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = nil
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
     }
 }
