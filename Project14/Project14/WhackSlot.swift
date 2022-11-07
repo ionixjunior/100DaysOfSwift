417a7b7148f1c7d61bbd5a8ab6edc979a8c05411
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-07 07:29:52 -0300

    Create a method to show the penguins

--- Project14/Project14/WhackSlot.swift
+++ Project14/Project14/WhackSlot.swift
@@ -3,6 +3,8 @@ import SpriteKit
 
 class WhackSlot: SKNode {
     var charNode: SKSpriteNode!
+    var isVisible = false
+    var isHit = false
     
     func configure(at position: CGPoint) {
         self.position = position
@@ -13,7 +15,7 @@ class WhackSlot: SKNode {
         let cropNode = SKCropNode()
         cropNode.position = CGPoint(x: 0, y: 15)
         cropNode.zPosition = 1
-        cropNode.maskNode = nil
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
         
         charNode = SKSpriteNode(imageNamed: "penguinGood")
         charNode.position = CGPoint(x: 0, y: -90)
@@ -21,5 +23,21 @@ class WhackSlot: SKNode {
         cropNode.addChild(charNode)
         
         addChild(cropNode)
+    }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
     }
 }
