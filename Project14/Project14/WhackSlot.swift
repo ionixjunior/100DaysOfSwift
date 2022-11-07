3a04fdc5c5e0ed140fd18409d4e5bcd3a95ef7d9
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-07 07:04:18 -0300

    Create WhackSlot class
    This class configure the position of the whack hole.

---
+++ Project14/Project14/WhackSlot.swift
@@ -0,0 +1,11 @@
+import UIKit
+import SpriteKit
+
+class WhackSlot: SKNode {
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+    }
+}
