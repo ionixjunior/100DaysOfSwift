11cc2d8de63ee33bb9916b17f7d8629de30907ca
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-07 06:59:14 -0300

    Add score

--- Project14/Project14/GameScene.swift
+++ Project14/Project14/GameScene.swift
@@ -1,12 +1,26 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "whackBackground")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+        
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.position = CGPoint(x: 8, y: 8)
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
