773f0e1069ff76280edc3e9013d4f2a220fa8ef5
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-09 07:15:36 -0300

    Add image into screen

--- Project15/Project15/ViewController.swift
+++ Project15/Project15/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var imageView: UIImageView!
+    var currentAnimation = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
     @IBAction func tapped(_ sender: Any) {
