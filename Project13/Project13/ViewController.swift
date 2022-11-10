ae3d10de4329925e6779441e290e88e2250cb556
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-10 07:27:55 -0300

    Add fade effect to show picked images

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -47,11 +47,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let image = info[.editedImage] as? UIImage else { return }
         dismiss(animated: true)
+        
+        imageView.alpha = 0
         currentImage = image
         
         let beginImage = CIImage(image: currentImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
         applyProcessing()
+        
+        UIView.animate(withDuration: 1, delay: 0) {
+            self.imageView.alpha = 1
+        }
     }
     
     func applyProcessing() {
