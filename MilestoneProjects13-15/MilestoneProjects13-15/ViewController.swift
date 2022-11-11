c7c4577f9ca3e9bd93d6a8790e8e10b941d00d44
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-11 07:47:19 -0300

    Implement numberOfRowsInSection and cellForRowAt

--- MilestoneProjects13-15/MilestoneProjects13-15/ViewController.swift
+++ MilestoneProjects13-15/MilestoneProjects13-15/ViewController.swift
@@ -1,12 +1,25 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var countries = [Country]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = countries[indexPath.row].name
+        cell.contentConfiguration = config
+        
+        return cell
+    }
 }
 
