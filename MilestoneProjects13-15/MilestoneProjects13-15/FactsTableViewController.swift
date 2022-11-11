931812437b1d980dd9ee16ef4b8d08baece9fbd6
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-11 20:10:41 -0300

    Show facts into facts view controller

--- MilestoneProjects13-15/MilestoneProjects13-15/FactsTableViewController.swift
+++ MilestoneProjects13-15/MilestoneProjects13-15/FactsTableViewController.swift
@@ -11,4 +11,36 @@ class FactsTableViewController: UITableViewController {
         }
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Fact", for: indexPath)
+        
+        let data = getData(from: indexPath.row)
+        var config = cell.defaultContentConfiguration()
+        config.text = data.name
+        config.secondaryText = data.description
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+    
+    private func getData(from: Int) -> (name: String, description: String) {
+        switch from {
+        case 0:
+            return ("Capital City", country.facts.capitalCity)
+        case 1:
+            return ("Official Languages", country.facts.officialLanguages.map {$0.name}.joined(separator: ", "))
+        case 2:
+            return ("Size", String(country.facts.size))
+        case 3:
+            return ("Population", String(country.facts.population))
+        case 4:
+            return ("Currency", country.facts.currency)
+        default:
+            return ("", "")
+        }
+    }
 }
