a6c4935242ad2624e91f57a217231075769b5912
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-11 19:53:22 -0300

    Add country property into facts view controller

--- MilestoneProjects13-15/MilestoneProjects13-15/FactsTableViewController.swift
+++ MilestoneProjects13-15/MilestoneProjects13-15/FactsTableViewController.swift
@@ -1,9 +1,14 @@
 import UIKit
 
 class FactsTableViewController: UITableViewController {
+    var country: Country!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let country = country {
+            title = country.name
+        }
     }
     
 }
