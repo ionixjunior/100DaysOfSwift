92428c803567d24e0235fca514447d790cef042c
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-12 15:53:58 -0300

    Add outlet of mapkit into view controller

--- Project16/Project16/ViewController.swift
+++ Project16/Project16/ViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
