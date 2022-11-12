1e10af25fe3c1d211781ba47928df54c2de55670
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-12 16:10:39 -0300

    Declare delegate of mapview into view controller

--- Project16/Project16/ViewController.swift
+++ Project16/Project16/ViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
