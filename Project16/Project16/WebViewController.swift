8ec0b491adbc3bd5aad2c7f3b0fd91c212a17eaf
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-13 11:14:30 -0300

    Add buttons to control webview
    These buttons can navigate to back, forward and refresh the page.

--- Project16/Project16/WebViewController.swift
+++ Project16/Project16/WebViewController.swift
@@ -17,6 +17,23 @@ class WebViewController: UIViewController {
         let request = URLRequest(url: url)
         webView.load(request)
         
+        let goBack = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(goBackTapped))
+        let goForward = UIBarButtonItem(title: "Forward", style: .plain, target: self, action: #selector(goForwardTapped))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(title: "Refresh", style: .plain, target: self, action: #selector(refreshTapped))
+        toolbarItems = [goBack, goForward, spacer, refresh]
         navigationController?.isToolbarHidden = false
+    }
+    
+    @objc func goBackTapped() {
+        webView.goBack()
+    }
+    
+    @objc func goForwardTapped() {
+        webView.goForward()
+    }
+    
+    @objc func refreshTapped() {
+        webView.reload()
     }
 }
