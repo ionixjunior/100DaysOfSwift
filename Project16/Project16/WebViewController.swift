f026991e00ecc1bea2b06e5ee91170156b236856
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-13 10:46:50 -0300

    Add a view controller with web view inside

---
+++ Project16/Project16/WebViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
