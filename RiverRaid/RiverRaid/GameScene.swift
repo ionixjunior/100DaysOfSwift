80102a0108d66545eb5496058410f1074c901555
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-15 14:58:38 -0300

    Create objects randomly

--- RiverRaid/RiverRaid/GameScene.swift
+++ RiverRaid/RiverRaid/GameScene.swift
@@ -2,6 +2,9 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var gameTimer: Timer?
+    var objects = ["helicopter", "ship"]
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 375, y: 667)
@@ -11,7 +14,20 @@ class GameScene: SKScene {
         
         let plane = SKSpriteNode(imageNamed: "plane")
         plane.position = CGPoint(x: 375, y: 100)
+        plane.physicsBody = SKPhysicsBody(texture: plane.texture!, size: plane.size)
+        plane.physicsBody?.affectedByGravity = false
         addChild(plane)
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(createObject), userInfo: nil, repeats: true)
+    }
+    
+    @objc func createObject() {
+        guard let object = objects.randomElement() else { return }
+        
+        let sprite = SKSpriteNode(imageNamed: object)
+        sprite.position = CGPoint(x: Int.random(in: 200...550), y: 1400)
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        addChild(sprite)
     }
 
     func touchMoved(toPoint pos : CGPoint) {
