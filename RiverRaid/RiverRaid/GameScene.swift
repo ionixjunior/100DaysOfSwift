d5bd977dcb8216f8f6ec05fc77d11560e0068a7a
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-15 14:47:39 -0300

    Add plane

--- RiverRaid/RiverRaid/GameScene.swift
+++ RiverRaid/RiverRaid/GameScene.swift
@@ -8,6 +8,10 @@ class GameScene: SKScene {
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+        
+        let plane = SKSpriteNode(imageNamed: "plane")
+        plane.position = CGPoint(x: 375, y: 100)
+        addChild(plane)
     }
 
     func touchMoved(toPoint pos : CGPoint) {
