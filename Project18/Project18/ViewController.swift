79443062c3ef776199f23867a1ff74245cfbe11b
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-16 06:31:16 -0300

    Remove unnecessary code

--- Project18/Project18/ViewController.swift
+++ Project18/Project18/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  Project18
-//
-//  Created by Ione Souza Junior on 16/11/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
