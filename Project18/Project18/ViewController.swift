7a8cf26bfdb48acd9ef1d244da747e40eb56f9dd
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-16 07:14:57 -0300

    Add some debug examples

--- Project18/Project18/ViewController.swift
+++ Project18/Project18/ViewController.swift
@@ -5,6 +5,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print(1, 2, 3, "teste", separator: "-", terminator: ":")
+        print("abc")
+        
+        for i in 1...100 {
+            print("The numer is", i)
+        }
+        
+        assert(1 == 1, "Math failure!")
+        assert(1 == 2, "Math failure!")
     }
 
 
