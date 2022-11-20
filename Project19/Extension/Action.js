7d6187487f9a4a4659f77a8485dd94a8776c3b81
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-20 13:15:55 -0300

    Add URL and title to a completion function

--- Project19/Extension/Action.js
+++ Project19/Extension/Action.js
@@ -2,7 +2,7 @@ var Action = function() { }
 
 Action.prototype = {
     run: function(parameters) {
-        
+        parameters.completionFunction({"URL": document.URL, "title": document.title})
     },
     
     finalize: function(parameters) {
