a19acf99743cc688619dc8503c17c7880edefa4e
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-20 13:50:01 -0300

    Create a button to dismiss the modal of extension

--- Project19/Extension/ActionViewController.swift
+++ Project19/Extension/ActionViewController.swift
@@ -12,6 +12,8 @@ class ActionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(done))
+        
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) {
@@ -30,7 +32,7 @@ class ActionViewController: UIViewController {
         }
     }
 
-    @IBAction func done() {
+    @objc func done() {
         // Return any edited content to the host app.
         // This template doesn't do anything, so we just echo the passed in items.
         self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
