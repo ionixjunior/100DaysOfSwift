2e7f0b162469b09635a7cfee33f0deb53f938552
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-23 07:29:36 -0300

    Implement a method to explode all selected fireworks

--- Project20/Project20/GameScene.swift
+++ Project20/Project20/GameScene.swift
@@ -140,4 +140,32 @@ class GameScene: SKScene {
         
         firework.removeFromParent()
     }
+    
+    func explodeFireworks() {
+        var numExploded = 0
+        
+        for (index, fireworkContainer) in fireworks.enumerated().reversed() {
+            guard let firework = fireworkContainer.children.first as? SKSpriteNode else { continue }
+            if firework.name == "selected" {
+                explode(firework: fireworkContainer)
+                fireworks.remove(at: index)
+                numExploded += 1
+            }
+        }
+        
+        switch numExploded {
+        case 0:
+            break
+        case 1:
+            score += 200
+        case 2:
+            score += 500
+        case 3:
+            score += 1500
+        case 4:
+            score += 2500
+        default:
+            score += 4000
+        }
+    }
 }
