73165d2f00671a7bc3061920e251d5312dc9d91f
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-24 20:17:56 -0300

    Add left and right navigation bar buttons

--- Project21/Project21/ViewController.swift
+++ Project21/Project21/ViewController.swift
@@ -5,8 +5,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
 
-
+    @objc func registerLocal() {
+        
+    }
+    
+    @objc func scheduleLocal() {
+        
+    }
 }
 
