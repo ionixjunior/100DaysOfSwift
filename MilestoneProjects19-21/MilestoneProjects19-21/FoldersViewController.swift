661c6767f468de9fc1a6b7979e2c4569c61e6463
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-26 09:02:58 -0300

    Enable large titles

--- MilestoneProjects19-21/MilestoneProjects19-21/FoldersViewController.swift
+++ MilestoneProjects19-21/MilestoneProjects19-21/FoldersViewController.swift
@@ -5,6 +5,7 @@ class FoldersViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Folders"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 
 
