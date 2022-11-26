0487cd30421f4a1c438dbc61bb48b77debae6e93
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-26 09:54:23 -0300

    Load data from json file

--- MilestoneProjects19-21/MilestoneProjects19-21/FoldersViewController.swift
+++ MilestoneProjects19-21/MilestoneProjects19-21/FoldersViewController.swift
@@ -12,10 +12,23 @@ class FoldersViewController: UIViewController {
         searchController.searchBar.placeholder = "Search"
         navigationItem.searchController = searchController
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editTapped))
+        
+        loadFolders()
     }
     
     @objc func editTapped() {
         
+    }
+    
+    func loadFolders() {
+        guard let url = Bundle.main.url(forResource: "folders", withExtension: "json") else { return }
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = try JSONDecoder().decode([Folder].self, from: data)
+            
+        } catch {
+            print(error)
+        }
     }
 }
 
