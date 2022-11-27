8a985d4a15af169092cfd9e7291a325a93403c22
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-27 11:46:29 -0300

    Implement a location manager extension
    This extension check the version of iOS to use the correctly way to start ranging beacons.

---
+++ Project22/Project22/Extensions/CLLocationManagerExtension.swift
@@ -0,0 +1,15 @@
+import CoreLocation
+
+extension CLLocationManager {
+    func start(uuid: UUID, major: CLBeaconMajorValue, minor: CLBeaconMinorValue, identifier: String) {
+        if #available(iOS 13.0, *) {
+            let beaconConstraint = CLBeaconIdentityConstraint(uuid: uuid, major: major, minor: minor)
+            self.startRangingBeacons(satisfying: beaconConstraint)
+            return
+        } else {
+            let beaconRegion = CLBeaconRegion(uuid: uuid, major: major, minor: minor, identifier: identifier)
+            self.startMonitoring(for: beaconRegion)
+            self.startRangingBeacons(in: beaconRegion)
+        }
+    }
+}
