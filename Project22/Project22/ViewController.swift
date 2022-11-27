2987a7061f86c51c1508af6619d320cabec24d11
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-27 10:44:29 -0300

    Add project 22

---
+++ Project22/Project22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Ione Souza Junior on 27/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
