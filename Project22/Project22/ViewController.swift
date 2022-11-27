56dfdcb1602b6bd2b9f590e21336f2afb2545a8c
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-27 10:52:30 -0300

    Add a label

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var distanceReading: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
