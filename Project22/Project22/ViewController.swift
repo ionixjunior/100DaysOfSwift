9e1207ccf5106d9e48321959a73316449518dc0c
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-27 10:55:35 -0300

    Instance a location manager into view controller

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -1,11 +1,16 @@
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var distanceReading: UILabel!
+    
+    var locationManager: CLLocationManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
     }
 
 
