7943229c7a95c411ea228b927cadd6ae470db3e0
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-27 12:11:24 -0300

    Remove unnecessary early return

--- Project22/Project22/Extensions/CLLocationManagerExtension.swift
+++ Project22/Project22/Extensions/CLLocationManagerExtension.swift
@@ -5,7 +5,6 @@ extension CLLocationManager {
         if #available(iOS 13.0, *) {
             let beaconConstraint = CLBeaconIdentityConstraint(uuid: uuid, major: major, minor: minor)
             self.startRangingBeacons(satisfying: beaconConstraint)
-            return
         } else {
             let beaconRegion = CLBeaconRegion(uuid: uuid, major: major, minor: minor, identifier: identifier)
             self.startMonitoring(for: beaconRegion)
