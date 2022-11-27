64b6d8031fb10184c43b290d0f8f03ae9075bb7d
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-27 10:53:44 -0300

    Remove unused code

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Project22
-//
-//  Created by Ione Souza Junior on 27/11/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,7 +5,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
