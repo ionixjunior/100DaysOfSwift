829b62247d9501c69283cb40835147b569302f39
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-28 08:25:54 -0300

    Add a circle to scale up or down when beacons is near by

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -4,6 +4,7 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var distanceReading: UILabel!
     @IBOutlet weak var beaconName: UILabel!
+    @IBOutlet weak var distanceView: UIView!
     
     var locationManager: CLLocationManager?
     var beacons: [(uuidString: String, major: CLBeaconMajorValue, minor: CLBeaconMajorValue, identifier: String)] = [
@@ -16,6 +17,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         distanceReading.text = "UNKNOWN"
         beaconName.text = ""
+        distanceView.layer.cornerRadius = 128
+        distanceView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
         
         locationManager = CLLocationManager()
         locationManager?.delegate = self
@@ -60,21 +63,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 self.view.backgroundColor = .blue
                 self.distanceReading.text = "FAR"
                 self.beaconName.text = identifier
+                self.distanceView.transform = CGAffineTransform(scaleX: 0.25, y: 0.25)
                 
             case .near:
                 self.view.backgroundColor = .orange
                 self.distanceReading.text = "NEAR"
                 self.beaconName.text = identifier
+                self.distanceView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
                 
             case .immediate:
                 self.view.backgroundColor = .red
                 self.distanceReading.text = "RIGHT HERE"
                 self.beaconName.text = identifier
+                self.distanceView.transform = CGAffineTransform(scaleX: 1, y: 1)
                 
             default:
                 self.view.backgroundColor = .gray
                 self.distanceReading.text = "UNKNOWN"
                 self.beaconName.text = ""
+                self.distanceView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
             }
         }
     }
