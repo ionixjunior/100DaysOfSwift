64e14fbf792fc42dd4b936961ca926aedb2c9730
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-29 07:00:15 -0300

    Add score

--- Project23/Project23/GameScene.swift
+++ Project23/Project23/GameScene.swift
@@ -2,6 +2,13 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var gameScore: SKLabelNode!
+    
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "sliceBackground")
@@ -12,5 +19,16 @@ class GameScene: SKScene {
         
         physicsWorld.gravity = CGVector(dx: 0, dy: -6)
         physicsWorld.speed = 0.85
+        
+        createScore()
+    }
+    
+    func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        gameScore.position = CGPoint(x: 8, y: 8)
+        score = 0
+        addChild(gameScore)
     }
 }
