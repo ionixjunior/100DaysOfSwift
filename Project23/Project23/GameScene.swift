2392a61a718f725d88fdbdcf408ae70bdc81f2ae
Ione Souza Junior <junior@ionixjunior.com.br>
2022-11-29 06:56:37 -0300

    Add a background

--- Project23/Project23/GameScene.swift
+++ Project23/Project23/GameScene.swift
@@ -4,6 +4,10 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.blendMode = .replace
+        background.position = CGPoint(x: 512, y: 384)
+        background.zPosition = -1
+        addChild(background)
     }
 }
