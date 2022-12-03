61896e9a29a96d135ff22e17db11b58aaf551b78
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-03 08:16:09 -0300

    Add some string extension samples

--- Day081.playground/Contents.swift
+++ Day081.playground/Contents.swift
@@ -1,1 +1,50 @@
+import Foundation
 
+extension String {
+    func withPrefix(_ prefix: String) -> String {
+        if self.hasPrefix(prefix) {
+            return self
+        }
+        
+        return prefix + self
+    }
+    
+    var isNumeric: Bool {
+        for letter in self {
+            if letter.isNumber == false {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    var lines: Array<String> {
+        return self.components(separatedBy: "\n")
+    }
+}
+
+"dislike".withPrefix("dis")
+"like".withPrefix("dis")
+
+
+"teste".isNumeric
+"te1te".isNumeric
+"123a".isNumeric
+"123".isNumeric
+
+
+"""
+loren
+""".lines
+
+"""
+loren
+ipsun
+""".lines
+
+"""
+loren
+ipsun
+dollor
+""".lines
