38da5d533a75d6fb27ff6cd72f5f7d67aad2dee0
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-04 16:00:28 -0300

    Add some extension samples

--- Day082.playground/Contents.swift
+++ Day082.playground/Contents.swift
@@ -1,2 +1,42 @@
 // Milestone of projects 22-24
 
+import UIKit
+
+extension UIView {
+    func bouceOut(duration: TimeInterval) {
+        UIView.animate(withDuration: duration, animations: {
+            view.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
+            view.backgroundColor = .red
+        })
+    }
+}
+
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+view.backgroundColor = .yellow
+view.bouceOut(duration: 2)
+
+
+extension Int {
+    func times (action: () -> Void) {
+        for _ in 1...self {
+            action()
+        }
+    }
+}
+
+3.times {
+    print("Hello world!")
+}
+
+
+extension Array where Element: Comparable {
+    mutating func remove(item: Element) {
+        if let index = self.firstIndex(of: item) {
+            self.remove(at: index)
+        }
+    }
+}
+
+var people = ["Samantha", "Taylor", "Laura", "Jorge", "Taylor"]
+people.remove(item: "Taylor")
+people
