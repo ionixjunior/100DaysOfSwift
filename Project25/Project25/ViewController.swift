82f2db906ca9c31e27fbca81851821aa6905def5
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-05 07:27:31 -0300

    Add a right bar button to choose an image

--- Project25/Project25/ViewController.swift
+++ Project25/Project25/ViewController.swift
@@ -5,8 +5,12 @@ class ViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Selfie Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(cameraTapped))
     }
-
-
+    
+    @objc func cameraTapped() {
+        
+    }
 }
 
