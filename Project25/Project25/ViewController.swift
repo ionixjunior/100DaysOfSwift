8faebe7764706f8c84b25f50f9e9dd169e1fd32e
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-05 07:30:57 -0300

    Implement collection view delegate

--- Project25/Project25/ViewController.swift
+++ Project25/Project25/ViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate {
+    var images = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,6 +12,20 @@ class ViewController: UICollectionViewController {
     
     @objc func cameraTapped() {
         
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+        
+        return cell
     }
 }
 
