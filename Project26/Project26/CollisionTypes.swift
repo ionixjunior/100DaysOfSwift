373f76473c4d8e51db83c45a221af2510d536780
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-07 07:19:35 -0300

    Implement the load level func

---
+++ Project26/Project26/CollisionTypes.swift
@@ -0,0 +1,7 @@
+enum CollisionTypes: UInt32 {
+    case player = 1
+    case wall = 2
+    case start = 4
+    case vortex = 8
+    case finish = 16
+}
--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -6,6 +6,67 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
+        loadLevel()
+    }
+    
+    func loadLevel() {
+        guard let levelURL = Bundle.main.url(forResource: "level1", withExtension: "txt") else { fatalError("Could not find level1.txt in the app bundle.") }
+        guard let levelString = try? String(contentsOf: levelURL) else { fatalError("Could not load level1.txt from the app bundle.") }
         
+        let lines = levelString.components(separatedBy: "\n")
+        
+        for (row, line) in lines.reversed().enumerated() {
+            for (column, letter) in line.enumerated() {
+                let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                
+                if letter == "x" {
+                    // load wall
+                    let node = SKSpriteNode(imageNamed: "block")
+                    node.position = position
+                    node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+                    node.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
+                    node.physicsBody?.isDynamic = false
+                    addChild(node)
+                } else if letter == "v" {
+                    // load vortex
+                    let node = SKSpriteNode(imageNamed: "vortex")
+                    node.name = "vortex"
+                    node.position = position
+                    node.run(SKAction.repeatForever(SKAction.rotate(byAngle: .pi, duration: 1)))
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.categoryBitMask = CollisionTypes.vortex.rawValue
+                    node.physicsBody?.isDynamic = false
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    addChild(node)
+                } else if letter == "s" {
+                    // load star
+                    let node = SKSpriteNode(imageNamed: "star")
+                    node.name = "star"
+                    node.position = position
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.categoryBitMask = CollisionTypes.start.rawValue
+                    node.physicsBody?.isDynamic = false
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    addChild(node)
+                } else if letter == "f" {
+                    // load finish point
+                    let node = SKSpriteNode(imageNamed: "finish")
+                    node.name = "finish"
+                    node.position = position
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.categoryBitMask = CollisionTypes.finish.rawValue
+                    node.physicsBody?.isDynamic = false
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    addChild(node)
+                } else if letter == " " {
+                    // empty space, do nothing
+                } else {
+                    fatalError("Unknown level letter: \(letter)")
+                }
+            }
+        }
     }
 }
