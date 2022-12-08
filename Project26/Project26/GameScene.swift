f53348166a3cd4185b4a8af62956531c55dbdf1e
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-08 06:59:31 -0300

    Remove unnecessary code

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -2,9 +2,6 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
