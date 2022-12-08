a83476819177edddc6c70fc6a75d3faf51232efb
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-08 07:24:08 -0300

    Extract load background to a function

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -8,12 +8,7 @@ class GameScene: SKScene {
     var motionManager: CMMotionManager?
     
     override func didMove(to view: SKView) {
-        let background = SKSpriteNode(imageNamed: "background")
-        background.position = CGPoint(x: 512, y: 384)
-        background.blendMode = .replace
-        background.zPosition = -1
-        addChild(background)
-        
+        loadBackground()
         loadLevel()
         createPlayer()
         
@@ -21,6 +16,14 @@ class GameScene: SKScene {
         
         motionManager = CMMotionManager()
         motionManager?.startAccelerometerUpdates()
+    }
+    
+    func loadBackground() {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
     }
     
     func loadLevel() {
