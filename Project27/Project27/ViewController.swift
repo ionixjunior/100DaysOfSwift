5e4a3d396eb67d1c80c635f311afc24b122affa7
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-10 16:47:02 -0300

    Add an image and a button

--- Project27/Project27/ViewController.swift
+++ Project27/Project27/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func redrawTapped(_ sender: Any) {
+    }
+    
 }
 
