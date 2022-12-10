fda6b94e1cb08d60188e59875225c48709f55e69
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-10 16:42:02 -0300

    Add project 27

---
+++ Project27/Project27/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by Ione Souza Junior on 10/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
