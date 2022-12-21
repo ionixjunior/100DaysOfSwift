b61c063344ae2ba5c00839392e5815fb009dba23
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-21 08:20:36 -0300

    Add bottom text

--- MilestoneProjects25-27/MilestoneProjects25-27/ViewController.swift
+++ MilestoneProjects25-27/MilestoneProjects25-27/ViewController.swift
@@ -51,6 +51,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
             }
             
             self?.imageView.image = imageWithText
+            self?.requestBottomText()
+        })
+        self.present(alert, animated: true)
+    }
+    
+    func requestBottomText() {
+        let alert = UIAlertController(title: "Enter the bottom text", message: nil, preferredStyle: .alert)
+        alert.addTextField()
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Add", style: .default) { [weak self] _ in
+            guard let text = alert.textFields?.first?.text else { return }
+            guard let image = self?.imageView.image else { return }
+            
+            let renderer = UIGraphicsImageRenderer(size: image.size)
+            let imageWithText = renderer.image { _ in
+                image.draw(at: CGPoint(x: 0, y: 0))
+                
+                let paragraphStyle = NSMutableParagraphStyle()
+                paragraphStyle.alignment = NSTextAlignment.center
+                
+                let attributes: [NSAttributedString.Key: Any] = [
+                    .font: UIFont.systemFont(ofSize: 100),
+                    .foregroundColor: UIColor.white,
+                    .paragraphStyle: paragraphStyle
+                ]
+                
+                let attributedString = NSAttributedString(string: text, attributes: attributes)
+                attributedString.draw(with: CGRect(x: 0, y: image.size.height - 140, width: image.size.width, height: image.size.height), options: .usesLineFragmentOrigin, context: nil)
+            }
+            
+            self?.imageView.image = imageWithText
         })
         self.present(alert, animated: true)
     }
