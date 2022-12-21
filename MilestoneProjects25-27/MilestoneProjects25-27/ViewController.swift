301f2b65191e093d057a53bb7037e7c7e4f3d657
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-21 07:45:29 -0300

    Add an image view into storyboard

--- MilestoneProjects25-27/MilestoneProjects25-27/ViewController.swift
+++ MilestoneProjects25-27/MilestoneProjects25-27/ViewController.swift
@@ -1,7 +1,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Meme generator"
