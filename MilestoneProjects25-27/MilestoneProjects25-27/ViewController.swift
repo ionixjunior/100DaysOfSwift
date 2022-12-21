775653a28d8718411670d71aac949f601ebf8d38
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-21 07:42:08 -0300

    Add right bar button

--- MilestoneProjects25-27/MilestoneProjects25-27/ViewController.swift
+++ MilestoneProjects25-27/MilestoneProjects25-27/ViewController.swift
@@ -5,8 +5,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Meme generator"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
     }
 
-
+    @objc func addTapped() {
+        
+    }
 }
 
