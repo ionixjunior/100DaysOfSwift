5cbce5c1c048fc012687bd367894a99ce1ec366c
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-21 08:12:25 -0300

    Add top text

--- MilestoneProjects25-27/MilestoneProjects25-27/ViewController.swift
+++ MilestoneProjects25-27/MilestoneProjects25-27/ViewController.swift
@@ -20,7 +20,39 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let image = info[.editedImage] as? UIImage else { return }
         imageView.image = image
-        picker.dismiss(animated: true)
+        picker.dismiss(animated: true) {
+            self.requestTopText()
+        }
+    }
+    
+    func requestTopText() {
+        let alert = UIAlertController(title: "Enter the top text", message: nil, preferredStyle: .alert)
+        alert.addTextField()
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Add", style: .default) { [weak self] _ in
+            guard let text = alert.textFields?.first?.text else { return }
+            guard let image = self?.imageView.image else { return }
+            
+            let renderer = UIGraphicsImageRenderer(size: image.size)
+            let imageWithText = renderer.image { _ in
+                image.draw(at: CGPoint(x: 0, y: 0))
+                
+                let paragraphStyle = NSMutableParagraphStyle()
+                paragraphStyle.alignment = NSTextAlignment.center
+                
+                let attributes: [NSAttributedString.Key: Any] = [
+                    .font: UIFont.systemFont(ofSize: 100),
+                    .foregroundColor: UIColor.white,
+                    .paragraphStyle: paragraphStyle
+                ]
+                
+                let attributedString = NSAttributedString(string: text, attributes: attributes)
+                attributedString.draw(with: CGRect(x: 0, y: 10, width: image.size.width, height: image.size.height), options: .usesLineFragmentOrigin, context: nil)
+            }
+            
+            self?.imageView.image = imageWithText
+        })
+        self.present(alert, animated: true)
     }
 }
 
