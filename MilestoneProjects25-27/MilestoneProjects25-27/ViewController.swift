9dcb462299da6d505f39e46dc7090bbb1fe12725
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-21 07:38:54 -0300

    Create milestone project 25-27

---
+++ MilestoneProjects25-27/MilestoneProjects25-27/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilestoneProjects25-27
+//
+//  Created by Ione Souza Junior on 21/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
