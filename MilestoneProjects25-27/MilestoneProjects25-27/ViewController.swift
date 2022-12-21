5f0069f46db471f5828b2ec61277d31c9de800fc
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-21 07:40:35 -0300

    Remove unnecessary code

--- MilestoneProjects25-27/MilestoneProjects25-27/ViewController.swift
+++ MilestoneProjects25-27/MilestoneProjects25-27/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  MilestoneProjects25-27
-//
-//  Created by Ione Souza Junior on 21/12/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
