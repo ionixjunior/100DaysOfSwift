3528527257f6840c7e4089a4baa845e216edf83c
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-27 19:39:09 -0300

    Add a text view

--- Project28/Project28/ViewController.swift
+++ Project28/Project28/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var secret: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
