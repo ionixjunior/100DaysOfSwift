aaeb1af1439b0d432413ede1bf5ce3230f33e5b5
Ione Souza Junior <junior@ionixjunior.com.br>
2023-01-14 20:59:35 -0300

    Create an UI test for flip all correct cards

--- MilestoneProjects28-30/MilestoneProjects28-30UITests/MilestoneProjects28_30UITests.swift
+++ MilestoneProjects28-30/MilestoneProjects28-30UITests/MilestoneProjects28_30UITests.swift
@@ -1,12 +1,27 @@
 import XCTest
 
 final class MilestoneProjects28_30UITests: XCTestCase {
-
+    var app: XCUIApplication!
+    
     override func setUpWithError() throws {
         continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDownWithError() throws {
+        app.terminate()
+        app = nil
     }
-    
+
+    func testFlipAllCorrectCards() throws {
+        let numberOfCards = 32
+        let numberOfPairs = numberOfCards / 2
+        
+        for cardGroup in 1...numberOfPairs {
+            let cardIdentifier = "card_\(cardGroup)"
+            app.buttons.matching(identifier: cardIdentifier).element(boundBy: 0).tap()
+            app.buttons.matching(identifier: cardIdentifier).element(boundBy: 1).tap()
+        }
+    }
 }
